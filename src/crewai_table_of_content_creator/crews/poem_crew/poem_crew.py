5172b2ff48d3cc524b1ab1db2e087e34a6b03d47
Syed safdar Ali Shah <programmersafdar@live.com>
2025-02-02 00:08:30 +0500

    inital

---
+++ src/crewai_table_of_content_creator/crews/poem_crew/poem_crew.py
@@ -0,0 +1,20 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+
+# If you want to run a snippet of code before or after the crew starts,
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+@CrewBase
+class BlogCrew:
+    """A crew that writes a blog post about a given topic."""
+    agents_config = '/config/agents.yaml'
+    tasks_config = '/config/tasks.yaml'
+
+    pass
+@CrewBase
+class ToCCrew:
+    """A crew that writes a blog Table of Contents about a given topic.""" 
+    agents_config = '/config/agents.yaml'
+    tasks_config = '/config/tasks.yaml'
+    pass
---
+++ src/crewai_table_of_content_creator/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from random import randint
+
+from pydantic import BaseModel
+
+from crewai.flow import Flow, listen, start
+
