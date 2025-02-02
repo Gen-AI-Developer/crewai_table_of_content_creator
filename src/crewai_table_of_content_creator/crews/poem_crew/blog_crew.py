19bc49011e7e62654a9a557b4ab31f5a4c54d934
Syed safdar Ali Shah <programmersafdar@live.com>
2025-02-02 09:45:10 +0500

    Crews / Agents / and / Tasks Added.

---
+++ src/crewai_table_of_content_creator/crews/poem_crew/blog_crew.py
@@ -0,0 +1,33 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+
+# If you want to run a snippet of code before or after the crew starts,
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+@CrewBase
+class BlogCrew:
+    """A crew that writes a blog post about a given Table of Contents."""
+    agents_config = '/config/agents.yaml'
+    tasks_config = '/config/tasks.yaml'
+
+    @agent
+    def blog_writer(self)-> Agent:
+        return Agent(
+            config = self.agents_config['blog_writer']
+        )
+    @task
+    def write_blog_post(self) -> Task:
+        return Task(
+            config = self.tasks_config['write_blog_post'],
+        )
+    @crew
+    def blog_crew(self) -> Crew:
+        return Crew (
+            agents=self.agents,
+            tasks=self.tasks,
+            process = Process.sequential,
+            verbos= True,
+            
+        )
+
---
+++ src/crewai_table_of_content_creator/crews/poem_crew/toc_crew.py
@@ -0,0 +1,31 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+
+# If you want to run a snippet of code before or after the crew starts,
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+@CrewBase
+class TocCrew:
+    """A crew that writes a blog Table of Contents about a given topic.""" 
+    agent_config = '/config/agents.yaml'
+    tasks_config = '/config/tasks.yaml'
+
+    @agent
+    def toc_writer(self)-> Agent:
+        return Agent(
+            config = self.agents_config['toc_writer']
+        )
+    @task
+    def write_toc(self) -> Task:
+        return Task(
+            config = self.tasks_config['write_toc'],
+        )
+    @crew
+    def toc_crew(self) -> Crew:
+        return Crew(
+                agent=self.agent,
+                task=self.task,
+                process=Process.sequential,
+                verbos = True,
+        )
